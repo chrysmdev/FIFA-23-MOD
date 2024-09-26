a7f8302734b7a424d3153b08eb44ada939da7604
Chrystian Menezes <chrys.dev21@gmail.com>
2024-09-26 00:50:33 -0300

    open mod manager working

--- main.py
+++ main.py
@@ -7,6 +7,14 @@ import pyautogui
 Program_Path_FE = "D:\JOGOS_COMPLEMENTOS\mods_fifa\FIFA.23.LE.v23.1.3.6\Launcher.exe"
 startfile(Program_Path_FE)
 
-# PASSING THROUGH THE USER ACCOUNT CONTROL
+# PASSING THROUGH DISCLAIMER
 sleep(1)
 pyautogui.press('enter')
+
+# OPEN MOD MANAGER
+Program_Path_MM = "D:\JOGOS_COMPLEMENTOS\mods_fifa\FIFA_Mod_Manager_v1.1.8\FIFA_Mod_Manager.exe.exe"
+startfile(Program_Path_MM)
+
+# LAUNCH FIFA 23 THROUGH MOD MANAGER
+sleep(3)
+pyautogui.doubleClick(780, 300)
