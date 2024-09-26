8791eceb76a508e0225b7ec197bc4a1367995c02
Chrystian Menezes <chrys.dev21@gmail.com>
2024-09-26 00:13:45 -0300

    testing space

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 # IMPORTS 
-import os
-import time
+from os import startfile
+from time import sleep
 import pyautogui
 
 # OPEN BOTH PROGRAMS
 Program_Path_MM = "D:\JOGOS_COMPLEMENTOS\mods_fifa\FIFA_Mod_Manager_v1.1.8\FIFA_Mod_Manager.exe.exe"
 Program_Path_FE = "D:\JOGOS_COMPLEMENTOS\mods_fifa\FIFA.23.LE.v23.1.3.6\Launcher.exe"
 
-os.startfile(Program_Path_MM)
-os.startfile(Program_Path_FE)
+startfile(Program_Path_MM)
+startfile(Program_Path_FE)
 
 # PASSING THROUGH THE USER ACCOUNT CONTROL
-time.sleep(2)
+sleep(2)
 pyautogui.press('left')
 pyautogui.press('enter')
