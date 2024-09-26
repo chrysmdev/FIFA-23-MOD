5eb1715a7cc9829bd775c524c518606e23eadf4a
Chrystian Menezes <chrys.dev21@gmail.com>
2024-09-26 00:18:44 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,11 +3,9 @@ from os import startfile
 from time import sleep
 import pyautogui
 
-# OPEN BOTH PROGRAMS
-Program_Path_MM = "D:\JOGOS_COMPLEMENTOS\mods_fifa\FIFA_Mod_Manager_v1.1.8\FIFA_Mod_Manager.exe.exe"
+# OPEN FILE EDITOR
 Program_Path_FE = "D:\JOGOS_COMPLEMENTOS\mods_fifa\FIFA.23.LE.v23.1.3.6\Launcher.exe"
 
-startfile(Program_Path_MM)
 startfile(Program_Path_FE)
 
 # PASSING THROUGH THE USER ACCOUNT CONTROL
