5b1db85df536ee02c6762b83139ea16c35d0044d
Chrystian Menezes <chrys.dev21@gmail.com>
2024-09-26 00:32:20 -0300

    Open File Editor working

--- main.py
+++ main.py
@@ -5,10 +5,8 @@ import pyautogui
 
 # OPEN FILE EDITOR
 Program_Path_FE = "D:\JOGOS_COMPLEMENTOS\mods_fifa\FIFA.23.LE.v23.1.3.6\Launcher.exe"
-
 startfile(Program_Path_FE)
 
 # PASSING THROUGH THE USER ACCOUNT CONTROL
-sleep(2)
-pyautogui.press('left')
+sleep(1)
 pyautogui.press('enter')
